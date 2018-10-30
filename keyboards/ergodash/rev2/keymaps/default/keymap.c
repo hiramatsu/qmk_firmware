10f9b7f20bb304c62ee42109f40aeed150ecafa0
yuichiro.hiramatsu <hiramatu@gmail.com>
2018-10-30 16:04:30 +0900

    Keymap_20181030

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL   , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_MINS  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT   , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-    KC_LCTL, KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC   , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSLS  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER  ,                        RAISE    , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
+    KC_LCTL, KC_LALT, KC_LGUI, EISU,             KC_DOWN, KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC   , KC_UP,            KC_LEFT, KC_RGHT, _______, KC_BSLS  \
   ),
 
   /* Lower
@@ -59,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LPRN,                        KC_RPRN, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-    KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_ENT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_DQT, \
-    KC_LCTL, KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_ENT , \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LOWER  ,                        RAISE  , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_DQT, \
+    KC_LCTL, KC_LALT, KC_LGUI, EISU,             KC_PGDN, KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC , KC_PGUP,          KC_HOME, KC_END, _______, _______   \
   ),
 
   /* Raise
@@ -80,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LPRN,                        KC_RPRN, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-    KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_MINS, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_ENT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_DQT, \
-    KC_LCTL, KC_LALT, KC_LGUI, EISU,             LOWER,   KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_ENT , \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LOWER  ,                        RAISE  , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_DQT, \
+    KC_LCTL, KC_LALT, KC_LGUI, EISU,             KC_PGDN, KC_SPC ,KC_LANG2,    KC_LANG1,  KC_SPC , KC_PGUP,          KC_HOME, KC_END, _______, _______   \
   ),
 
   /* Adjust
